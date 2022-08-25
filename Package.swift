9a1ae8b7c690495f20a1e4a53922a30c47a92c45
Tomás <moyanotomasi@gmail.com>
2022-08-25 17:35:07 +0200

    Removed buggy Progress package

--- Package.swift
+++ Package.swift
@@ -9,24 +9,24 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(
-          name: "MKRingProgressViewPackage",
-          targets: ["MKRingProgressViewPackage"]),
+//        .library(
+//          name: "MKRingProgressViewPackage",
+//          targets: ["MKRingProgressViewPackage"]),
       
         .library(
           name: "MyFrameworkPackage",
           targets: ["MyFrameworkPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/maxkonovalov/MKRingProgressView.git", .upToNextMajor(from: "2.3.0")),
+//        .package(url: "https://github.com/maxkonovalov/MKRingProgressView.git", .upToNextMajor(from: "2.3.0")),
         .package(url: "https://github.com/Tomoy/MyFrameworkPackage.git", .upToNextMajor(from: "0.0.0"))
     ],
     targets: [
-        .target(
-          name: "MKRingProgressViewPackage",
-          dependencies: [
-            .product(name: "MKRingProgressView", package: "MKRingProgressView"),
-          ]),
+//        .target(
+//          name: "MKRingProgressViewPackage",
+//          dependencies: [
+//            .product(name: "MKRingProgressView", package: "MKRingProgressView"),
+//          ]),
         
           .target(
             name: "MyFrameworkPackage",
