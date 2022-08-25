bd81dfa831f2be836fdcb2595c12c5d1bcc53efd
Tomás <moyanotomasi@gmail.com>
2022-08-25 17:30:58 +0200

    First commit

--- Package.swift
+++ Package.swift
@@ -5,24 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "TomasPackage",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TomasPackage",
-            targets: ["TomasPackage"]),
+          name: "MKRingProgressViewPackage",
+          targets: ["MKRingProgressViewPackage"]),
+      
+        .library(
+          name: "MyFrameworkPackage",
+          targets: ["MyFrameworkPackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/maxkonovalov/MKRingProgressView.git", .upToNextMajor(from: "2.3.0")),
+        .package(url: "https://github.com/Tomoy/MyFrameworkPackage.git", .upToNextMajor(from: "0.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TomasPackage",
-            dependencies: []),
-        .testTarget(
-            name: "TomasPackageTests",
-            dependencies: ["TomasPackage"]),
+          name: "MKRingProgressViewPackage",
+          dependencies: [
+            .product(name: "MKRingProgressView", package: "MKRingProgressView"),
+          ]),
+        
+          .target(
+            name: "MyFrameworkPackage",
+            dependencies: [
+              .product(name: "MyFrameworkPackage", package: "MyFrameworkPackage"),
+            ]),
     ]
 )
